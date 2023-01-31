2e8669d8686bf27aa6fa78c517a1aa4bfb78f0cc
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-01-31 19:21:42 +0200

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!(Number.isInteger(num1) && Number.isInteger(num2))) return 'ERROR';
+    if(num1 < 0 || num2 < 0) return 'ERROR';
 
+    let result = 0;
+    let edges = (num1 < num2) ? [num1, num2] : [num2, num1];
+
+    for(let i = edges[0]; i <= edges[1]; i++) result+=i;
+    return result;
 };
 
 // Do not edit below this line
