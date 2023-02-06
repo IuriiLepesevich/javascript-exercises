d5c14decedf090b42f52949fde4c3b59c5d57c2b
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-02-06 19:20:15 +0200

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, elem) => total + (elem), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return (arr.length) ? arr.reduce((total, elem) => total * (elem), 1)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(!num) return 1;
+  total = 1;
+  for(let i = num; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
