48f04b4dfba90bc7efaad7003067133f2f3b73d3
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-02-06 21:01:18 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((obj, elem) => {
+        if(!obj.yearOfBirth) obj = elem;
+        
+        deathDateElem = (elem.yearOfDeath) ? elem.yearOfDeath :
+        (new Date).getFullYear();
 
+        deathDateObj = (obj.yearOfDeath) ? obj.yearOfDeath :
+        (new Date).getFullYear();
+
+        if((deathDateElem - elem.yearOfBirth) > (deathDateObj - obj.yearOfBirth)) {
+            obj = elem;
+        }
+        return obj;
+    }, {})
 };
 
 // Do not edit below this line
