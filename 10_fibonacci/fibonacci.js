92120a6dd6aed153e0cc6f762c100876270fd66c
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-02-06 20:25:01 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return 'OOPS';
+    if(num === 0) return 0;
 
+    num = parseInt(num);
+    let a = 1;
+    let b = 1;
+    for(let i = 2; i < num; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
